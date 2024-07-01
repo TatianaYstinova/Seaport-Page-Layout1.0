7f0a459e22a986e9b7181f7e5f7f7d5eaf854b2e
ustinova <rykova.tanya.96@gmail.com>
2024-07-01 15:59:57 +0300

    12

--- slider-test.js
+++ slider-test.js
@@ -1,18 +1,50 @@
+const newCards = [
+    {
+        picture: 'https://i.1.creatium.io/14/59/2d/7a1c484a5de50e5c436b26554e2ad9cb70/nastol.com_.ua-223487.jpg',
+        data: '01/03/2024',
+        headerNews: 'Semper eu pulvinar eget integer',
+        descriptionNews: 'Pretium duis phasellus netus ac. Nunc nibh nunc integer feugiat et aliquam cras. Amet pharetra montes ipsum gravida tellus tellus.',
 
+    },
+    {
+        picture: 'https://flagman-news.ru/news/2022050411_cover.jpg',
+        data: '25/06/2024',
+        headerNews: 'Semper eu pulvinar eget integer',
+        descriptionNews: 'Pretium duis phasellus netus ac. Nunc nibh nunc integer feugiat et aliquam cras. Amet pharetra montes ipsum gravida tellus tellus.',
 
+    },
+    {
+        picture: 'https://www.infranews.ru/wp-content/uploads/2016/04/port.jpg',
+        data: '25/03/2023',
+        headerNews: 'Semper eu pulvinar eget integer',
+        descriptionNews: 'Pretium duis phasellus netus ac. Nunc nibh nunc integer feugiat et aliquam cras. Amet pharetra montes ipsum gravida tellus tellus.',
+
+    },
+    {
+        picture: 'https://prognosist.ru/wp-content/uploads/2023/05/nmtp-4.jpg',
+        data: '25/03/2023',
+        headerNews: 'Semper eu pulvinar eget integer',
+        descriptionNews: 'Pretium duis phasellus netus ac. Nunc nibh nunc integer feugiat et aliquam cras. Amet pharetra montes ipsum gravida tellus tellus.',
+
+    }
+];
 
 const newsCardContainer = document.querySelector('.news-card-container');
-
-for(let i = 0; i < newsCardContainer.children.length; i++){
+for (let i=0;i<newCards.length;i++) {
     const newDiv = document.createElement('div');
     newsCardContainer.appendChild(newDiv);
     newDiv.outerHTML = `
-    <div class="newsletter">
-    <img  class='img-news' src="./img/новость1.png"/>
-    <div class="news-date">
-    <div class="text-news">
-        <div class="header-news">Semper eu pulvinar eget integer</div>
-        <div class="description-news">Pretium duis phasellus netus ac. Nunc nibh nunc integer feugiat et aliquam cras. Amet pharetra montes ipsum gravida tellus tellus.</div>
-        </div>`
-
-}
+        <div class="news-card">
+        <img  class='img-news' src="${newCards[i].picture}"/>
+        <div class="news-date">${newCards[i].data}</div>
+        <div class="header-news">${newCards[i].headerNews}</div>
+        <div class="description-news">${newCards[i].descriptionNews}</div>
+        </div>
+        `
+    
+}    
+$(document).ready(function(){
+    $('.news-card-container').slick({
+      setting-name: setting-value
+    });
+  });
